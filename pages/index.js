8b37365e7ec0881924e15dea2f8096df95b6ff88
Nghia Tran <vinhnghiatran@gmail.com>
2022-01-15 10:31:48 +0700

    Add tailwind css

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
