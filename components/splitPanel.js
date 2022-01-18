939fa90435cf9830ab9c07031ae4e22222088465
Nghia Tran <vinhnghiatran@gmail.com>
2022-01-18 11:02:19 +0700

    Use Allotment

--- components/splitPanel.js
+++ components/splitPanel.js
@@ -1,20 +1,20 @@
-import SplitPane from "react-split-pane/lib/SplitPane";
-import Pane from "react-split-pane/lib/Pane";
 import Console from "./console";
 import MonacoEditor from "./monacoEditor";
 import App from "../model/app";
+import dynamic from "next/dynamic";
+import "allotment/dist/style.css";
+const Allotment = dynamic(
+  () => import("allotment").then((mod) => mod.Allotment),
+  { ssr: false }
+);
 
 export default function SplitPanel() {
   const app = new App();
 
   return (
-    <SplitPane split="vertical">
-      <Pane initialSize="60%" minSize="30%">
-        <MonacoEditor onRunCallback={app.onRunCallback} onShareCallback={app.onShareCallBack}/>
-      </Pane>
-      <Pane minSize="15%">
-        <Console></Console>
-      </Pane>
-    </SplitPane>
+    <Allotment minSize={300}>
+      <MonacoEditor onRunCallback={app.onRunCallback} onShareCallback={app.onShareCallBack}/>
+      <Console></Console>
+    </Allotment>
   );
 }
